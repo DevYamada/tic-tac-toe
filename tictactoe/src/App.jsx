ab089b9f078e345a56e5e1f1ef88e3b35b80f378
Lucas Yamada <{email}>
2025-03-20 02:12:42 -0300

    development phase 1

---
+++ tictactoe/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Columns from './components/columns'
+
+function App() {
+
+  return (
+    <>
+      <Columns />
+    </>
+  )
+}
+
+export default App
