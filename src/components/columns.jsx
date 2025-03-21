43ee902425634a16133ad35dee6977f9f973ba43
Lucas Yamada <{email}>
2025-03-20 22:47:21 -0300

    bot development phase 1

--- src/components/columns.jsx
+++ src/components/columns.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, use } from "react";
 
 function Columns() {
   const [grid, setGrid] = useState({
@@ -13,12 +13,62 @@ function Columns() {
     bottomRight: "h",
   });
 
+  const [gridKeys, setGridKeys] = useState([
+    "topLeft",
+    "topMiddle",
+    "topRight",
+    "middleLeft",
+    "middleMiddle",
+    "middleRight",
+    "bottomLeft",
+    "bottomMiddle",
+    "bottomRight",
+  ]);
+
   const [selectedPlayer, setSelectedPlayer] = useState("x");
+  const [selectedPlayerBot, setSelectedPlayerBot] = useState("c");
+  const [bot, setBot] = useState(false);
+
+  const randomNumber = () => {
+    let random = Math.floor(Math.random() * 9);
+    return random;
+  };
+
+  const botPlay = () => {
+    console.log('---------------------')
+    let botPlayPosition = randomNumber();
+    while (grid[gridKeys[botPlayPosition]] !== "h") {
+      console.log(grid[gridKeys[botPlayPosition]]);
+      botPlayPosition = randomNumber();
+    }
+    if (grid[gridKeys[botPlayPosition]] !== "h") {
+      botPlay();
+    } else {
+      console.log(botPlayPosition);
+      console.log(grid[gridKeys[botPlayPosition]]);
+      botPlayPosition = gridKeys[botPlayPosition];
+      setGrid((prev) => ({
+        ...prev,
+        [botPlayPosition]: `${selectedPlayerBot}`,
+      }));
+    }
+  };
+
+  const callBotPlay = () => {
+    setTimeout(() => {
+      botPlay()
+    }, 500);
+
+  }
 
   const play = (position) => {
+    /*let na = gridKeys[4]*/
+
     if (grid[position] == "h") {
       setGrid((prev) => ({ ...prev, [position]: `${selectedPlayer}` }));
-      setSelectedPlayer((prev) => (prev === "x" ? "c" : "x"));
+      /*setSelectedPlayer((prev) => (prev === "x" ? "c" : "x"));*/
+
+      callBotPlay()
     }
   };
 
